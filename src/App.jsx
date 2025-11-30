f248ff260b6c75651e7cabdead2bd568de7b7f09
dvankevich <dvankevich@gmail.com>
2025-11-30 18:49:04 +0200

    State Object Update

--- src/App.jsx
+++ src/App.jsx
@@ -41,6 +41,29 @@ function App() {
     return <button onClick={handleClick}>Current: {clicks}</button>;
   };
 
+  const ClickCounterSync = () => {
+    return <button onClick={handleClick}>Current: {clicks}</button>;
+  };
+
+  const [values, setValues] = useState({
+    x: 0,
+    y: 0,
+  });
+
+  const updateX = () => {
+    setValues({
+      ...values,
+      x: values.x + 1,
+    });
+  };
+
+  const updateY = () => {
+    setValues({
+      ...values,
+      y: values.y + 1,
+    });
+  };
+
   return (
     <>
       <div>
@@ -73,6 +96,20 @@ function App() {
         <h2>Independent Click Counter</h2>
         <ClickCounter />
         <ClickCounter />
+      </div>
+      <div>
+        <h2>Synchronized Click Counter</h2>
+        <ClickCounterSync />
+        <ClickCounterSync />
+      </div>
+      <div>
+        <h2>State Object Update</h2>
+        <p>
+          x: {values.x}, y: {values.y}
+        </p>
+
+        <button onClick={updateX}>Update x</button>
+        <button onClick={updateY}>Update y</button>
       </div>
     </>
   );
