f217be9d123fe3485577f5bded8de1a2cde0155b
dima <dvankevich@gmail.com>
2025-12-03 13:33:37 +0200

    unmounting phase

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,26 @@
 import "./App.css";
 import { useState, useEffect } from "react";
 
+const Modal = () => {
+  useEffect(() => {
+    // Зберігаємо ідентифікатор інтервалу в змінну
+    const intervalId = setInterval(() => {
+      console.log(`Interval ID: ${intervalId}, Time: ${Date.now()}`);
+    }, 2000);
+
+    return () => {
+      // Очищаємо інтервал за його ідентифікатором
+      console.log(`clean Interval with ID: ${intervalId}`);
+      clearInterval(intervalId);
+    };
+  }, []);
+
+  return <div>Modal</div>;
+};
+
 const App = () => {
   const [clicks, setClicks] = useState(0);
+  const [isOpen, setIsOpen] = useState(false);
 
   // Оголошуємо ефект
   useEffect(() => {
@@ -14,9 +32,17 @@ const App = () => {
   }, []); // Виконується тільки на етапі монтування.
 
   return (
-    <button onClick={() => setClicks(clicks + 1)}>
-      You clicked {clicks} times
-    </button>
+    <>
+      <button onClick={() => setClicks(clicks + 1)}>
+        You clicked {clicks} times
+      </button>
+      <div>
+        <button onClick={() => setIsOpen(!isOpen)}>
+          {isOpen ? "Close" : "Open"}
+        </button>
+        {isOpen && <Modal />}
+      </div>
+    </>
   );
 };
 
