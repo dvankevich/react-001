a4439ddbe464dd1e9c6696a9c598050dcf1eb845
dima <dvankevich@gmail.com>
2025-12-03 13:06:03 +0200

    mounting phase

--- src/App.jsx
+++ src/App.jsx
@@ -1,118 +1,23 @@
 import "./App.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
-function Product() {
-  return (
-    <div>
-      <h2>Cookies</h2>
-      <p>Price: 999 credits</p>
-    </div>
-  );
-}
-
-const MyButton = () => {
-  return <button onClick={() => alert("I'm a button!")}>Click me!</button>;
-};
-
-const CustomButton = ({ message, children }) => {
-  return <button onClick={() => alert(message)}>{children}</button>;
-};
-
-const ClickCounterSync = ({ value, onUpdate }) => {
-  return <button onClick={onUpdate}>Current: {value}</button>;
-};
-
-function App() {
+const App = () => {
   const [clicks, setClicks] = useState(0);
-  const [isOpen, setIsOpen] = useState(false);
 
-  const handleClick = (evt) => {
-    console.log(evt);
-    setClicks(clicks + 1);
-  };
-
-  const handleToggle = () => {
-    setIsOpen(!isOpen);
-  };
-
-  const ClickCounter = () => {
-    const [clicks, setClicks] = useState(0);
-
-    const handleClick = () => {
-      setClicks(clicks + 1);
-    };
-
-    return <button onClick={handleClick}>Current: {clicks}</button>;
-  };
-
-  const [values, setValues] = useState({
-    x: 0,
-    y: 0,
+  // Оголошуємо ефект
+  useEffect(() => {
+    document.title = `You clicked ${clicks} times`;
   });
 
-  const updateX = () => {
-    setValues({
-      ...values,
-      x: values.x + 1,
-    });
-  };
-
-  const updateY = () => {
-    setValues({
-      ...values,
-      y: values.y + 1,
-    });
-  };
+  useEffect(() => {
+    console.log("You can see me only once!");
+  }, []); // Виконується тільки на етапі монтування.
 
   return (
-    <>
-      <div>
-        <h1>Products</h1>
-
-        <Product />
-        <MyButton />
-        <button onClick={() => alert("You clicked me!")}>
-          Click me again!!!
-        </button>
-      </div>
-      <div>
-        <button onClick={handleClick}>First button with handleClick</button>
-        <button onClick={(evt) => console.log(evt)}>Second button</button>
-      </div>
-      <div>
-        <CustomButton message="Playing music!">Play some music</CustomButton>
-        <CustomButton message="Uploading your data!">Upload data</CustomButton>
-      </div>
-      <div>
-        <h2>Click counter</h2>
-        <button onClick={handleClick}>Current: {clicks}</button>
-      </div>
-      <div>
-        <h2>Toggle section</h2>
-        <button onClick={handleToggle}>{isOpen ? "Hide" : "Show"}</button>
-        {isOpen && <p>Now you can see me!</p>}
-      </div>
-      <div>
-        <h2>Independent Click Counter</h2>
-        <ClickCounter />
-        <ClickCounter />
-      </div>
-      <div>
-        <h2>Synchronized Click Counter</h2>
-        <ClickCounterSync value={clicks} onUpdate={handleClick} />
-        <ClickCounterSync value={clicks} onUpdate={handleClick} />
-      </div>
-      <div>
-        <h2>State Object Update</h2>
-        <p>
-          x: {values.x}, y: {values.y}
-        </p>
-
-        <button onClick={updateX}>Update x</button>
-        <button onClick={updateY}>Update y</button>
-      </div>
-    </>
+    <button onClick={() => setClicks(clicks + 1)}>
+      You clicked {clicks} times
+    </button>
   );
-}
+};
 
 export default App;
