3d4b78a56e19ad5e373b77db2755137c8d28dc10
dvankevich <dvankevich@gmail.com>
2025-12-03 18:14:16 +0200

    multiple effects

--- src/App.jsx
+++ src/App.jsx
@@ -31,6 +31,21 @@ const App = () => {
     console.log("You can see me only once!");
   }, []); // Виконується тільки на етапі монтування.
 
+  const [first, setFirst] = useState(0);
+  const [second, setSecond] = useState(0);
+
+  useEffect(() => {
+    console.log("First updated: ", first);
+  }, [first]);
+
+  useEffect(() => {
+    console.log("Second updated: ", second);
+  }, [second]);
+
+  useEffect(() => {
+    console.log("First or second updated: ", first + second);
+  }, [first, second]);
+
   return (
     <>
       <button onClick={() => setClicks(clicks + 1)}>
@@ -41,6 +56,10 @@ const App = () => {
           {isOpen ? "Close" : "Open"}
         </button>
         {isOpen && <Modal />}
+      </div>
+      <div>
+        <button onClick={() => setFirst(first + 1)}>First: {first}</button>
+        <button onClick={() => setSecond(second + 1)}>Second: {second}</button>
       </div>
     </>
   );
