0f235f76bc43305877bf177f7f5b7f609b347f72
dvankevich <dvankevich@gmail.com>
2025-12-04 20:53:28 +0200

    select element

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { useState } from "react";
+import { useState, useId } from "react";
 
 const SearchBar = () => {
   const [inputValue, setInputValue] = useState("");
@@ -16,12 +16,37 @@ const SearchBar = () => {
   );
 };
 
+const LangSwitcher = ({ value, onSelect }) => {
+  const selectId = useId();
+
+  return (
+    <div>
+      <label htmlFor={selectId}>Choose language</label>
+      <select
+        id={selectId}
+        value={value}
+        onChange={(evt) => onSelect(evt.target.value)}
+      >
+        <option value="uk">Ukrainian</option>
+        <option value="en">English</option>
+        <option value="pl">Polish</option>
+      </select>
+    </div>
+  );
+};
+
 function App() {
+  const [lang, setLang] = useState("uk");
   return (
     <>
       <div>
         <h2>Search bar</h2>
         <SearchBar />
+      </div>
+      <div>
+        <h2>Language switcher</h2>
+        <p>Selected language: {lang}</p>
+        <LangSwitcher value={lang} onSelect={setLang} />
       </div>
     </>
   );
