f06efa1699b380228b12e5b0e9b1bc9d94809190
dima <dvankevich@gmail.com>
2025-12-04 12:32:29 +0200

    simple form

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,44 @@
 import "./App.css";
 
+const LoginForm = ({ onLogin }) => {
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+
+    const form = evt.target;
+    const { login, password } = form.elements;
+    console.log("form.elements", form.elements);
+
+    // Викликаємо пропс onLogin
+    onLogin({
+      login: login.value,
+      password: password.value,
+    });
+
+    form.reset();
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="text" name="login" />
+      <input type="password" name="password" />
+      <button type="submit">Login</button>
+    </form>
+  );
+};
+
+const handleLogin = (userData) => {
+  // Виконуємо необхідні операції з даними
+  console.log(userData);
+};
+
 function App() {
   return (
     <>
-      <h1>Forms</h1>
+      <div>
+        <h1>Please login to your account!</h1>
+        {/* Передаємо колбек як пропс форми */}
+        <LoginForm onLogin={handleLogin} />
+      </div>
     </>
   );
 }
