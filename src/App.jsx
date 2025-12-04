dbdd6de6ffd0c858f5a0f6ac43d7dc12070c98f4
dvankevich <dvankevich@gmail.com>
2025-12-04 20:32:17 +0200

    save data to local storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,65 +1,34 @@
 import "./App.css";
 import { useState, useEffect } from "react";
 
-const Modal = () => {
-  useEffect(() => {
-    // Зберігаємо ідентифікатор інтервалу в змінну
-    const intervalId = setInterval(() => {
-      console.log(`Interval ID: ${intervalId}, Time: ${Date.now()}`);
-    }, 2000);
-
-    return () => {
-      // Очищаємо інтервал за його ідентифікатором
-      console.log(`clean Interval with ID: ${intervalId}`);
-      clearInterval(intervalId);
-    };
-  }, []);
-
-  return <div>Modal</div>;
-};
-
 const App = () => {
-  const [clicks, setClicks] = useState(0);
-  const [isOpen, setIsOpen] = useState(false);
+  const [clicks, setClicks] = useState(() => {
+    // Зчитуємо значення за ключем
+    const savedClicks = Number(window.localStorage.getItem("saved-clicks"));
 
-  // Оголошуємо ефект
+    // Якщо там щось є, повертаємо це
+    // значення як початкове значення стану
+    if (savedClicks !== null) {
+      return savedClicks;
+    }
+
+    // У протилежному випадку повертаємо
+    // яке-небудь значення за замовчуванням
+    return 0;
+  });
+
   useEffect(() => {
-    document.title = `You clicked ${clicks} times`;
+    console.log(`Clicks changed - ${clicks}`);
+    window.localStorage.setItem("saved-clicks", clicks);
   }, [clicks]);
-
-  useEffect(() => {
-    console.log("You can see me only once!");
-  }, []); // Виконується тільки на етапі монтування.
-
-  const [first, setFirst] = useState(0);
-  const [second, setSecond] = useState(0);
-
-  useEffect(() => {
-    console.log("First updated: ", first);
-  }, [first]);
-
-  useEffect(() => {
-    console.log("Second updated: ", second);
-  }, [second]);
-
-  useEffect(() => {
-    console.log("First or second updated: ", first + second);
-  }, [first, second]);
 
   return (
     <>
-      <button onClick={() => setClicks(clicks + 1)}>
-        You clicked {clicks} times
-      </button>
       <div>
-        <button onClick={() => setIsOpen(!isOpen)}>
-          {isOpen ? "Close" : "Open"}
+        <button onClick={() => setClicks(clicks + 1)}>
+          You clicked {clicks} times
         </button>
-        {isOpen && <Modal />}
-      </div>
-      <div>
-        <button onClick={() => setFirst(first + 1)}>First: {first}</button>
-        <button onClick={() => setSecond(second + 1)}>Second: {second}</button>
+        <button onClick={() => setClicks(0)}>Reset</button>
       </div>
     </>
   );
