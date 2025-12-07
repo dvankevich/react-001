57b5bbc5e4ff15090e221dc12091d9cc6e5141d6
dvankevich <dvankevich@gmail.com>
2025-12-07 18:44:13 +0200

    formik yup

--- src/App.jsx
+++ src/App.jsx
@@ -1,164 +1,86 @@
 import "./App.css";
-import { useState, useId } from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import { useId } from "react";
+import * as Yup from "yup";
 
-const SearchBar = () => {
-  const [inputValue, setInputValue] = useState("");
+const FeedbackSchema = Yup.object().shape({
+  username: Yup.string()
+    .min(2, "Too Short!")
+    .max(50, "Too Long!")
+    .required("Required"),
+  email: Yup.string().email("Must be a valid email!").required("Required"),
+  message: Yup.string()
+    .min(3, "Too short")
+    .max(256, "Too long")
+    .required("Required"),
+  level: Yup.string().oneOf(["good", "neutral", "bad"]).required("Required"),
+});
 
-  const handleChange = (evt) => {
-    setInputValue(evt.target.value);
-  };
-
-  return (
-    <div>
-      <input type="text" value={inputValue} onChange={handleChange} />
-      <p>{inputValue}</p>
-    </div>
-  );
+const initialValues = {
+  username: "",
+  email: "",
+  message: "",
+  level: "good",
 };
 
-const LangSwitcher = ({ value, onSelect }) => {
-  const selectId = useId();
+const FeedbackForm = () => {
+  const nameFieldId = useId();
+  const emailFieldId = useId();
+  const msgFieldId = useId();
+  const levelFieldId = useId();
 
-  return (
-    <div>
-      <label htmlFor={selectId}>Choose language</label>
-      <select
-        id={selectId}
-        value={value}
-        onChange={(evt) => onSelect(evt.target.value)}
-      >
-        <option value="uk">Ukrainian</option>
-        <option value="en">English</option>
-        <option value="pl">Polish</option>
-      </select>
-    </div>
-  );
-};
-
-const LoginForm = () => {
-  const [values, setValues] = useState({
-    login: "",
-    password: "",
-  });
-
-  const handleChange = (evt) => {
-    setValues({
-      ...values,
-      [evt.target.name]: evt.target.value,
-    });
-  };
-
-  const handleSumit = (evt) => {
-    evt.preventDefault();
-
+  const handleSubmit = (values, actions) => {
     console.log(values);
-
-    setValues({
-      login: "",
-      password: "",
-    });
+    actions.resetForm();
   };
 
   return (
-    <form className="simpleForm" onSubmit={handleSumit}>
-      <input
-        type="text"
-        name="login"
-        value={values.login}
-        onChange={handleChange}
-      />
-      <input
-        type="password"
-        name="password"
-        value={values.password}
-        onChange={handleChange}
-      />
-      <button type="submit">Login</button>
-    </form>
+    <Formik
+      initialValues={initialValues}
+      onSubmit={handleSubmit}
+      validationSchema={FeedbackSchema}
+    >
+      <Form>
+        <div>
+          <label htmlFor={nameFieldId}>Username</label>
+          <Field type="text" name="username" id={nameFieldId} />
+          <ErrorMessage name="username" component="span" />
+        </div>
+
+        <div>
+          <label htmlFor={emailFieldId}>Email</label>
+          <Field type="email" name="email" id={emailFieldId} />
+          <ErrorMessage name="email" component="span" />
+        </div>
+
+        <div>
+          <label htmlFor={msgFieldId}>Message</label>
+          <Field as="textarea" name="message" id={msgFieldId} rows="5" />
+          <ErrorMessage name="message" component="span" />
+        </div>
+
+        <div>
+          <label htmlFor={levelFieldId}>Service satisfaction level</label>
+          <Field as="select" name="level" id={levelFieldId}>
+            <option value="good">Good</option>
+            <option value="neutral">Neutral</option>
+            <option value="bad">Bad</option>
+          </Field>
+          <ErrorMessage name="level" component="span" />
+        </div>
+
+        <button type="submit">Submit</button>
+      </Form>
+    </Formik>
   );
 };
 
 function App() {
-  const [lang, setLang] = useState("uk");
-  const [coffeeSize, setCoffeeSize] = useState("sm");
-
-  const handleSizeChange = (evt) => {
-    setCoffeeSize(evt.target.value);
-  };
-
-  const [hasAccepted, setHasAccepted] = useState(false);
-
-  const handleChange = (evt) => {
-    setHasAccepted(evt.target.checked);
-  };
-
   return (
     <>
       <div>
-        <h2>Search bar</h2>
-        <SearchBar />
-      </div>
-      <div>
-        <h2>Language switcher</h2>
-        <p>Selected language: {lang}</p>
-        <LangSwitcher value={lang} onSelect={setLang} />
-      </div>
-      <div>
-        <h2>Select coffee size</h2>
-        <label>
-          <input
-            type="radio"
-            name="coffeeSize"
-            value="sm"
-            checked={coffeeSize === "sm"}
-            onChange={handleSizeChange}
-          />
-          Small
-        </label>
-        <label>
-          <input
-            type="radio"
-            name="coffeeSize"
-            value="md"
-            checked={coffeeSize === "md"}
-            onChange={handleSizeChange}
-          />
-          Meduim
-        </label>
-        <label>
-          <input
-            type="radio"
-            name="coffeeSize"
-            value="lg"
-            checked={coffeeSize === "lg"}
-            onChange={handleSizeChange}
-          />
-          Large
-        </label>
-      </div>
-      <div>
-        <h2>CheckBox</h2>
-        <label>
-          <input
-            type="checkbox"
-            name="terms"
-            checked={hasAccepted}
-            onChange={handleChange}
-          />
-          I accept terms and conditions
-        </label>
-        <button
-          type="button"
-          title="accept terms"
-          disabled={!hasAccepted}
-          onClick={() => console.log("Proceed next")}
-        >
-          Proceed
-        </button>
-      </div>
-      <div>
-        <h2>Login form</h2>
-        <LoginForm />
+        <h2>Formik</h2>
+        <FeedbackForm />
       </div>
     </>
   );
