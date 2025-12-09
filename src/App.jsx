838eca19bacb203424dd4ce3108e4cd4a1ffe840
dvankevich <dvankevich@gmail.com>
2025-12-09 20:39:47 +0200

    simple player

--- src/App.jsx
+++ src/App.jsx
@@ -1,89 +1,29 @@
 import "./App.css";
-import { Formik, Form, Field, ErrorMessage } from "formik";
-import { useId } from "react";
-import * as Yup from "yup";
 
-const FeedbackSchema = Yup.object().shape({
-  username: Yup.string()
-    .min(2, "Too Short!")
-    .max(50, "Too Long!")
-    .required("Required"),
-  email: Yup.string().email("Must be a valid email!").required("Required"),
-  message: Yup.string()
-    .min(3, "Too short")
-    .max(256, "Too long")
-    .required("Required"),
-  level: Yup.string().oneOf(["good", "neutral", "bad"]).required("Required"),
-});
+import { useRef } from "react";
 
-const initialValues = {
-  username: "",
-  email: "",
-  message: "",
-  level: "good",
-};
+const Player = ({ source }) => {
+  const playerRef = useRef();
 
-const FeedbackForm = () => {
-  const nameFieldId = useId();
-  const emailFieldId = useId();
-  const msgFieldId = useId();
-  const levelFieldId = useId();
+  const play = () => playerRef.current.play();
 
-  const handleSubmit = (values, actions) => {
-    console.log(values);
-    actions.resetForm();
-  };
+  const pause = () => playerRef.current.pause();
 
   return (
-    <Formik
-      initialValues={initialValues}
-      onSubmit={handleSubmit}
-      validationSchema={FeedbackSchema}
-    >
-      <Form>
-        <div>
-          <label htmlFor={nameFieldId}>Username</label>
-          <Field type="text" name="username" id={nameFieldId} />
-          <ErrorMessage name="username" component="span" />
-        </div>
-
-        <div>
-          <label htmlFor={emailFieldId}>Email</label>
-          <Field type="email" name="email" id={emailFieldId} />
-          <ErrorMessage name="email" component="span" />
-        </div>
-
-        <div>
-          <label htmlFor={msgFieldId}>Message</label>
-          <Field as="textarea" name="message" id={msgFieldId} rows="5" />
-          <ErrorMessage name="message" component="span" />
-        </div>
-
-        <div>
-          <label htmlFor={levelFieldId}>Service satisfaction level</label>
-          <Field as="select" name="level" id={levelFieldId}>
-            <option value="good">Good</option>
-            <option value="neutral">Neutral</option>
-            <option value="bad">Bad</option>
-          </Field>
-          <ErrorMessage name="level" component="span" />
-        </div>
-
-        <button type="submit">Submit</button>
-      </Form>
-    </Formik>
-  );
-};
-
-function App() {
-  return (
-    <>
+    <div>
+      <video ref={playerRef} src={source}>
+        Sorry, your browser does not support embedded videos.
+      </video>
       <div>
-        <h2>Formik</h2>
-        <FeedbackForm />
+        <button onClick={play}>Play</button>
+        <button onClick={pause}>Pause</button>
       </div>
-    </>
+    </div>
   );
-}
+};
+
+const App = () => {
+  return <Player source="http://media.w3.org/2010/05/sintel/trailer.mp4" />;
+};
 
 export default App;
