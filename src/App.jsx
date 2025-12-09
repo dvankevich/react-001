a21d16535e87572889dd4d64a6c1f0dddeed9931
dvankevich <dvankevich@gmail.com>
2025-12-09 17:34:51 +0200

    ArticleList

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,40 @@
 import "./App.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+const ArticleList = ({ items }) => (
+  <ul>
+    {items.map(({ objectID, url, title }) => (
+      <li key={objectID}>
+        <a href={url} target="_blank" rel="noreferrer noopener">
+          {title}
+        </a>
+      </li>
+    ))}
+  </ul>
+);
 
 function App() {
+  // 1. Оголошуємо стан
+  const [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    async function fetchArticles() {
+      const response = await axios.get(
+        "https://hn.algolia.com/api/v1/search?query=react"
+      );
+      // 2. Записуємо дані в стан
+      setArticles(response.data.hits);
+    }
+
+    fetchArticles();
+  }, []);
+
   return (
-    <>
-      <div>
-        <h2>HTTP requests</h2>
-      </div>
-    </>
+    <div>
+      <h1>Latest articles</h1>
+      {articles.length > 0 && <ArticleList items={articles} />}
+    </div>
   );
 }
 
